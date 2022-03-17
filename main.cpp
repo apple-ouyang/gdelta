db6df16d32b6eda92e0f8433b4e2d502690b03b9
i404788 <404788@student.fontys.nl>
2022-03-17 12:41:39 +0100

    Format

--- main.cpp
+++ main.cpp
@@ -62,21 +62,22 @@ int main(int argc, char *argv[]) {
   }
 
   if (edflags > 2 || edflags == 0) {
-usage:
-    fprintf(stderr, "Usage: gdelta [-d|-e] [-o <outputfile>] <basefile> <delta|target-file> \n");
+  usage:
+    fprintf(stderr, "Usage: gdelta [-d|-e] [-o <outputfile>] <basefile> "
+                    "<delta|target-file> \n");
     return 1;
   }
 
   for (int i = 0; optind < argc; i++, optind++) {
     switch (i) {
-	case 0:
-		basefp = argv[optind];
-		break;
-	case 1:
-		targetfp = argv[optind];
-		break;
-	default:
-		goto usage;
+    case 0:
+      basefp = argv[optind];
+      break;
+    case 1:
+      targetfp = argv[optind];
+      break;
+    default:
+      goto usage;
     }
   }
 
@@ -84,8 +85,7 @@ usage:
          edflags & 0b10);
 
   if (basefp == nullptr || targetfp == nullptr)
-	goto usage;
-
+    goto usage;
 
   // Set output filedescriptor (stdout or file)
   int output_fd = fileno(stdout);
@@ -93,55 +93,55 @@ usage:
 #ifdef _WIN32
     output_fd = open(cvalue, O_RDWR | O_TRUNC | O_CREAT);
 #else
-    output_fd = open(cvalue, O_RDWR | O_TRUNC | O_CREAT, S_IRGRP | S_IWGRP | S_IWUSR | S_IRUSR);
+    output_fd = open(cvalue, O_RDWR | O_TRUNC | O_CREAT,
+                     S_IRGRP | S_IWGRP | S_IWUSR | S_IRUSR);
 #endif
-    if (output_fd < 0)
-    {
+    if (output_fd < 0) {
       printf("Failed to open output file (%d)\n", output_fd);
       return 1;
     }
   }
-
 
   uint8_t *target_delta;
   uint64_t target_delta_size = load_file_to_memory(targetfp, &target_delta);
   uint8_t *origin;
   uint64_t origin_size = load_file_to_memory(basefp, &origin);
 
-  if (edflags & 0b10){
-	// Encode target, origin -> delta
-	
-	// Maximum size for delta is the target state (since it's only useful if it's less than that) 
-	uint32_t delta_size = target_delta_size;
-	uint8_t *delta = (uint8_t*)malloc(delta_size);
-	int status = gencode(target_delta, target_delta_size, origin, origin_size, delta, &delta_size);
+  if (edflags & 0b10) {
+    // Encode target, origin -> delta
 
+    // Maximum size for delta is the target state (since it's only useful if
+    // it's less than that)
+    uint32_t delta_size = target_delta_size;
+    uint8_t *delta = (uint8_t *)malloc(delta_size);
+    int status = gencode(target_delta, target_delta_size, origin, origin_size,
+                         delta, &delta_size);
 
-	if(write(output_fd, delta, delta_size) < 0){
-    printf("Failed to write output file (%zd)", output_fd);
-    return 1;
-  }
+    if (write(output_fd, delta, delta_size) < 0) {
+      printf("Failed to write output file (%zd)\n", output_fd);
+      return 1;
+    }
 
-	free(delta);
-	return status;
+    free(delta);
+    return status;
   }
 
   if (edflags & 0b01) {
-	// Decode origin, delta -> target
-	// Allocate slightly more than the origin and delta combined
-	// TODO: handle status and increase buffer if too small
-	uint32_t target_size = target_delta_size + origin_size * 11 / 10;
-	uint8_t *target = (uint8_t*)malloc(target_size);
-	int status = gdecode(target_delta, target_delta_size, origin, origin_size, target, &target_size);
+    // Decode origin, delta -> target
+    // Allocate slightly more than the origin and delta combined
+    // TODO: handle status and increase buffer if too small
+    uint32_t target_size = target_delta_size + origin_size * 11 / 10;
+    uint8_t *target = (uint8_t *)malloc(target_size);
+    int status = gdecode(target_delta, target_delta_size, origin, origin_size,
+                         target, &target_size);
 
+    if (write(output_fd, target, target_size) < 0) {
+      printf("Failed to write output file (%zd)\n", output_fd);
+      return 1;
+    }
 
-	if(write(output_fd, target, target_size) < 0){
-    printf("Failed to write output file (%zd)", output_fd);
-    return 1;
-  }
-
-	free(target);
-	return status;
+    free(target);
+    return status;
   }
 
   free(target_delta);
@@ -149,7 +149,7 @@ usage:
 
   if (output_fd != fileno(stdout)) {
     close(output_fd);
-  } 
+  }
 
   return 0;
 }
